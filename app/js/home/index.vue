1bb1bf4bd1d4718afdb51f7b61cc51aef0512af1
sela <yanling.shi@midea.com>
2020-05-05 16:58:53 +0800

    feature:init commit，setup

---
+++ app/js/home/index.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Description: 
+ * @Author: Sela
+ * @Date: 2018-01-28 22:57:44
+ * @LastEditTime: 2020-05-05 10:34:30
+ * @LastEditors:  
+ -->
+<template lang="html">
+    <div :class="$style.home">
+        <h1>Home</h1>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" module>
+  @import '../../css/reset.scss';
+  .home{
+    color: red;
+    font-size: 20px;
+  }
+</style>
